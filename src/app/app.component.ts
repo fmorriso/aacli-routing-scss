304ca0e6ef8249822d376b86f83cbe50b5cf25ae
Fred Morrison <fpmorrison@verizon.net>
2019-02-17 09:14:12 -0500

    upgraded to Angular 7.2.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, VERSION} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aacli-routing-scss';
+  angularVersion: string;
+
+  constructor() {
+    this.angularVersion = VERSION.full;
+  }
 }
