10eec3d2244c5108cf42df68c38a222392c827b6
Fred Morrison <fpmorrison@verizon.net>
2020-03-15 17:30:20 -0400

    fix indentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-
 const routes: Routes = [
 	{ path: 'home', component: HomeComponent, data: { label: 'Home' } },
 	{ path: 'about', component: AboutComponent, data: { label: 'About' } },
